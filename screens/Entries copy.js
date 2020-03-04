0c951a1fb7d579d333628e5be2215b5d5bf7de1c
Juan Pablo Salazar <pablo.salazar.restrepo@gmail.com>
2020-03-04 16:31:11 -0500

    search by plate and show results

---
+++ screens/Entries copy.js
@@ -0,0 +1,226 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+
+import { Button, Input, Select, Loader } from '../components';
+
+//API
+import { registerCustomer } from '../API/CusotmerService';
+import { registerVehicle } from '../API/VehicleService';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Este campo es requerido'),
+  document_type: Yup.string()
+    .required('Este campo es requerido'),
+  document_number: Yup.string()
+    .label('Número de documento')
+    .required('Este campo es requerido'),
+  phone: Yup.string()
+    .label('Telefono')
+    .required('Este campo es requerido'),
+  plate: Yup.string()
+    .label('Placa')
+    .required('Este campo es requerido'),
+  brand: Yup.string()
+    .label('Marca')
+    .required('Este campo es requerido'),
+  color: Yup.string()
+    .label('Color')
+    .required('Este campo es requerido'),
+})
+
+const document_type_list = [
+  { label: 'CC - Cédula de ciudadanía', value: 'CC' },
+  { label: 'TI - Tarjeta de identidad', value: 'TI' },
+  { label: 'CE - Cédula de extranjería', value: 'CE' },
+];
+
+const brand_list = [
+  { label: 'KIA', value: 'KIA' },
+  { label: 'Chevrolet', value: 'Chevrolet' },
+  { label: 'Mazda', value: 'Mazda' },
+]
+
+export default class Entries extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        name: '',
+        document_type: '',
+        document_number: '',
+        phone: '',
+        plate: '',
+        brand: '',
+        color: '',
+      },
+      error: null
+    };
+  }
+
+  handleSubmit = async (data) => {
+    try {
+      this.setState({ isLoading: true });
+      const customer = await registerCustomer(data);
+      const vehicle = await registerVehicle({
+        ...data,
+        customer_id: customer.id,
+      });
+      
+    } catch (error) {
+      this.setState( error );
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    const { data, error, isLoading } = this.state;
+    return (
+      <ScrollView>
+        <View style={{flex: 1, paddingHorizontal: 30 }}>
+          {isLoading && <Loader />}
+          {error && <Text>{error}</Text>}
+          <Formik
+              initialValues={data}
+              onSubmit={values => {this.handleSubmit(values)}}
+              validationSchema={validationSchema}
+            >
+              {({ handleChange, values, handleSubmit, errors, isValid, isSubmitting, touched, handleBlur }) => (
+                <>
+                  {error && <Text style={styles.title}>{error}</Text>}
+                  <Text style={styles.title}>INFO CLIENTE</Text>
+                  <Input
+                    name='name'
+                    label="Nombre del cliente"
+                    value={values.name}
+                    handleChange={handleChange}
+                    error={errors.name}
+                  />
+
+                  <Select
+                    name='document_type'
+                    items={document_type_list}
+                    label="Tipo de documento"
+                    value={values.document_type}
+                    handleChange={handleChange}
+                    error={errors.document_type}
+                  />
+                  
+                  <Input
+                    name='document_number'
+                    label="Número de documento"
+                    value={values.document_number}
+                    handleChange={handleChange}
+                    error={errors.document_number}
+                  />
+
+                  <Input
+                    name='phone'
+                    label="Teléfono del cliente"
+                    value={values.phone}
+                    handleChange={handleChange}
+                    error={errors.phone}
+                  />
+
+                  <Text style={styles.title}>INFO VEHÍCULO</Text>
+
+                  <Input
+                    name='plate'
+                    label="Placa del vehículo"
+                    value={values.plate}
+                    handleChange={handleChange}
+                    error={errors.plate}
+                  />
+
+                  <Select
+                    name='brand'
+                    items={brand_list}
+                    label="Marca del vehiculo"
+                    value={values.brand}
+                    handleChange={handleChange}
+                    error={errors.brand}
+                  />
+
+                  <Input
+                    name='color'
+                    label="Color del vehículo"
+                    value={values.color}
+                    handleChange={handleChange}
+                    error={errors.color}
+                  />
+
+                  {/* <Text style={styles.title}>ESTADO VEHÍCULO</Text>
+
+                  <Input
+                    name='plate'
+                    label="Color del vehículo"
+                    multiline
+                    numberOfLines={5}
+                    value={values.plate}
+                    handleChange={handleChange}
+                    error={errors.plate}
+                  /> */}
+
+             
+                  <View paddingVertical={5} />
+                  <Button
+                    label="REGISTRAR"
+                    handleSubmit={handleSubmit}
+                    // disabled={isLoading}
+                  />
+                  <View paddingVertical={20} />
+                </>
+              )}
+          </Formik>
+        </View>
+      </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: '#b98700',
+    marginVertical: 20,
+    fontSize: 20,
+    borderBottomWidth: 1,
+    borderColor: '#b98700',
+  },
+  textInput: {
+    height: 50,
+    color: '#969696',
+    paddingLeft: 15,
+    borderWidth: 1,
+    borderColor: '#b98700',
+    fontSize: 16,
+  },
+  buttonPrimary: {
+    backgroundColor: "#b98700",
+    marginTop: 40,
+    paddingHorizontal: 40,
+  },
+  errorText: {
+    color: '#c43d4b',
+    marginBottom: 5, 
+  },
+  textDanger: {
+    paddingHorizontal: 20,
+    color: '#c43d4b',
+  },
+  alertDanger: {
+    backgroundColor: 'rgba(196, 61, 75, .2)',
+    marginBottom: 20,
+    paddingVertical: 14,
+    fontSize: 14,
+    borderWidth: 1,
+    borderColor: '#c43d4b1a',
+  },
+  textPrimary: {
+    color: '#b98700',
+    textAlign: 'center'
+  },
+})
+
--- screens/Entries.js
+++ screens/Entries.js
@@ -1,182 +1,127 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, ScrollView } from 'react-native';
-import * as Yup from 'yup';
-import { Formik } from 'formik';
+import { SearchBar, ListItem, FlatList } from 'react-native-elements';
+import TouchableScale from 'react-native-touchable-scale'; 
+import { Avatar } from 'react-native-elements';
 
-import { Button, Input, Select, Loader } from '../components';
+import { Button } from '../components';
+import { getVehicleByPlate } from '../API/VehicleService';
 
-//API
-import { registerCustomer } from '../API/CusotmerService';
-import { registerVehicle } from '../API/VehicleService';
 
-const validationSchema = Yup.object().shape({
-  name: Yup.string()
-    .required('Este campo es requerido'),
-  document_type: Yup.string()
-    .required('Este campo es requerido'),
-  document_number: Yup.string()
-    .label('Número de documento')
-    .required('Este campo es requerido'),
-  phone: Yup.string()
-    .label('Telefono')
-    .required('Este campo es requerido'),
-  plate: Yup.string()
-    .label('Placa')
-    .required('Este campo es requerido'),
-  brand: Yup.string()
-    .label('Marca')
-    .required('Este campo es requerido'),
-  color: Yup.string()
-    .label('Color')
-    .required('Este campo es requerido'),
-})
-
-const document_type_list = [
-  { label: 'CC - Cédula de ciudadanía', value: 'CC' },
-  { label: 'TI - Tarjeta de identidad', value: 'TI' },
-  { label: 'CE - Cédula de extranjería', value: 'CE' },
-];
-
-const brand_list = [
-  { label: 'KIA', value: 'KIA' },
-  { label: 'Chevrolet', value: 'Chevrolet' },
-  { label: 'Mazda', value: 'Mazda' },
-]
 
 export default class Entries extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {
-        name: '',
-        document_type: '',
-        document_number: '',
-        phone: '',
-        plate: '',
-        brand: '',
-        color: '',
-      },
-      error: null
+      search: '',
+      step: 1,
+      spinner: false,
+      vehicleList: [],
+      customerList: [],
     };
+    // this.keyExtractor = (item, index) => index.toString()
   }
 
-  handleSubmit = async (data) => {
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+    if (search.length >= 2) {
+      this.searchByPlate(search);
+    } else {
+      this.setState({ spinner: false, error: null })
+    }
+  };
+
+  searchByPlate = async (search) => {
+    this.setState({spinner: true});
     try {
-      this.setState({ isLoading: true });
-      const customer = await registerCustomer(data);
-      const vehicle = await registerVehicle({
-        ...data,
-        customer_id: customer.id,
-      });
-      
+      response = await getVehicleByPlate(search.trim());
+      this.setState({
+        vehicleList: response.data,
+      })
     } catch (error) {
-      this.setState( error );
+      this.setState({error, vehicleList: [] });
     } finally {
-      this.setState({ isLoading: false });
+      this.setState({spinner: false});
     }
   }
 
+  handlePressVehicle = (index) => {
+    const { vehicleList } = this.state;
+    this.setState({
+      plateSelected: vehicleList[index].plate,
+      customerList: vehicleList[index].customers,
+      isVisible: true,
+    })
+  }
+
   render() {
-    const { data, error, isLoading } = this.state;
+    const { step, search, spinner, vehicleList } = this.state;
     return (
-      <ScrollView>
-        <View style={{flex: 1, paddingHorizontal: 30 }}>
-          {isLoading && <Loader />}
-          {error && <Text>{error}</Text>}
-          <Formik
-              initialValues={data}
-              onSubmit={values => {this.handleSubmit(values)}}
-              validationSchema={validationSchema}
-            >
-              {({ handleChange, values, handleSubmit, errors, isValid, isSubmitting, touched, handleBlur }) => (
-                <>
-                  {error && <Text style={styles.title}>{error}</Text>}
-                  <Text style={styles.title}>INFO CLIENTE</Text>
-                  <Input
-                    name='name'
-                    label="Nombre del cliente"
-                    value={values.name}
-                    handleChange={handleChange}
-                    error={errors.name}
-                  />
+      <View style={{flex: 1, paddingHorizontal: 30 }}>
+        <Text style={styles.title}>PASO 1</Text>
+        <SearchBar
+          placeholder="Digite la placa..."
+          onChangeText={this.updateSearch}
+          value={search}
+          showLoading={spinner}
+          inputStyle={styles.textUppercase}
+        />
 
-                  <Select
-                    name='document_type'
-                    items={document_type_list}
-                    label="Tipo de documento"
-                    value={values.document_type}
-                    handleChange={handleChange}
-                    error={errors.document_type}
-                  />
-                  
-                  <Input
-                    name='document_number'
-                    label="Número de documento"
-                    value={values.document_number}
-                    handleChange={handleChange}
-                    error={errors.document_number}
-                  />
-
-                  <Input
-                    name='phone'
-                    label="Teléfono del cliente"
-                    value={values.phone}
-                    handleChange={handleChange}
-                    error={errors.phone}
-                  />
-
-                  <Text style={styles.title}>INFO VEHÍCULO</Text>
-
-                  <Input
-                    name='plate'
-                    label="Placa del vehículo"
-                    value={values.plate}
-                    handleChange={handleChange}
-                    error={errors.plate}
-                  />
-
-                  <Select
-                    name='brand'
-                    items={brand_list}
-                    label="Marca del vehiculo"
-                    value={values.brand}
-                    handleChange={handleChange}
-                    error={errors.brand}
-                  />
-
-                  <Input
-                    name='color'
-                    label="Color del vehículo"
-                    value={values.color}
-                    handleChange={handleChange}
-                    error={errors.color}
-                  />
-
-                  {/* <Text style={styles.title}>ESTADO VEHÍCULO</Text>
-
-                  <Input
-                    name='plate'
-                    label="Color del vehículo"
-                    multiline
-                    numberOfLines={5}
-                    value={values.plate}
-                    handleChange={handleChange}
-                    error={errors.plate}
-                  /> */}
-
-             
-                  <View paddingVertical={5} />
-                  <Button
-                    label="REGISTRAR"
-                    handleSubmit={handleSubmit}
-                    // disabled={isLoading}
-                  />
-                  <View paddingVertical={20} />
-                </>
-              )}
-          </Formik>
-        </View>
-      </ScrollView>
+        <Text style={styles.plate}>{search}</Text>
+        {search.length > 2 && (
+          vehicleList.length === 0 ? (
+            <>
+              <Text style={styles.found}>NO SE ENCONTRARON RESULTADOS</Text>
+              {search.length < 5 && <Text style={styles.found}>Escribe al menos 5 caracteres para continuar...</Text>}
+            </>
+          ) : (
+            <>
+              <Text style={styles.found}>SE ENCONTRÓ {vehicleList.length} RESULTADOS</Text>
+              <ScrollView>
+                {
+                  vehicleList.map((item, i) => (
+                    
+                    <ListItem
+                      key={i}
+                      Component={TouchableScale}
+                      friction={90}
+                      tension={100}
+                      activeScale={0.95}
+                      title={item.plate}
+                      titleStyle={{ fontWeight: 'bold' }}
+                      subtitleStyle={{ textTransform: 'uppercase' }}
+                      subtitle={`${item.brand} - ${item.color}`}
+                      disabled={spinner}
+                      leftIcon={
+                        <Avatar
+                          size="medium"
+                          overlayContainerStyle={{backgroundColor: '#000'}}
+                          rounded 
+                          icon={{ name: 'car', color: '#b98700', type: 'material-community' }} 
+                        />
+                      }
+                      bottomDivider
+                      chevron
+                    />
+                  ))
+                }
+              </ScrollView>
+              <View style={{marginBottom: 30}} />
+            </>
+          )
+        )}
+        {search.length > 4 && vehicleList.length === 0 && (
+          <Button
+            label="Continuar"
+            // handleSubmit={handleSubmit}
+            // disabled={isLoading}
+          />
+        )}
+      </View>
     )
   }
 }
@@ -189,38 +134,22 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#b98700',
   },
-  textInput: {
-    height: 50,
-    color: '#969696',
-    paddingLeft: 15,
-    borderWidth: 1,
-    borderColor: '#b98700',
-    fontSize: 16,
-  },
-  buttonPrimary: {
-    backgroundColor: "#b98700",
-    marginTop: 40,
-    paddingHorizontal: 40,
-  },
-  errorText: {
-    color: '#c43d4b',
-    marginBottom: 5, 
-  },
-  textDanger: {
-    paddingHorizontal: 20,
-    color: '#c43d4b',
-  },
-  alertDanger: {
-    backgroundColor: 'rgba(196, 61, 75, .2)',
-    marginBottom: 20,
-    paddingVertical: 14,
-    fontSize: 14,
-    borderWidth: 1,
-    borderColor: '#c43d4b1a',
-  },
-  textPrimary: {
+  plate: {
     color: '#b98700',
-    textAlign: 'center'
+    fontWeight: "400",
+    fontSize: 36,
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    marginVertical: 10,
   },
+  found: {
+    color: '#969696',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  textUppercase: {
+    textTransform: 'uppercase',
+    color: '#ffffff'
+  }
 })
 
