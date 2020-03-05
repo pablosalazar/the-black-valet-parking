fdfff6101b8a9ce246ae3de037a92d80cb5f43b2
Juan Pablo Salazar <pablo.salazar.restrepo@gmail.com>
2020-03-05 12:11:23 -0500

    list customers of a vehicle in vehicles entry

--- screens/Entries.js
+++ screens/Entries.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, ScrollView } from 'react-native';
-import { SearchBar, ListItem, FlatList } from 'react-native-elements';
+import { SearchBar, ListItem, Badge } from 'react-native-elements';
 import TouchableScale from 'react-native-touchable-scale'; 
 import { Avatar } from 'react-native-elements';
 
 import { Button } from '../components';
 import { getVehicleByPlate } from '../API/VehicleService';
-
-
 
 export default class Entries extends Component {
   constructor(props) {
@@ -19,7 +17,6 @@ export default class Entries extends Component {
       vehicleList: [],
       customerList: [],
     };
-    // this.keyExtractor = (item, index) => index.toString()
   }
 
   updateSearch = search => {
@@ -49,80 +46,123 @@ export default class Entries extends Component {
     }
   }
 
-  handlePressVehicle = (index) => {
+  handleSelectVehicle = (index) => {
     const { vehicleList } = this.state;
     this.setState({
       plateSelected: vehicleList[index].plate,
       customerList: vehicleList[index].customers,
-      isVisible: true,
+      step: 2,
     })
   }
 
   render() {
-    const { step, search, spinner, vehicleList } = this.state;
-    return (
-      <View style={{flex: 1, paddingHorizontal: 30 }}>
-        <Text style={styles.title}>PASO 1</Text>
-        <SearchBar
-          placeholder="Digite la placa..."
-          onChangeText={this.updateSearch}
-          value={search}
-          showLoading={spinner}
-          inputStyle={styles.textUppercase}
-        />
-
-        <Text style={styles.plate}>{search}</Text>
-        {search.length > 2 && (
-          vehicleList.length === 0 ? (
-            <>
-              <Text style={styles.found}>NO SE ENCONTRARON RESULTADOS</Text>
-              {search.length < 5 && <Text style={styles.found}>Escribe al menos 5 caracteres para continuar...</Text>}
-            </>
-          ) : (
-            <>
-              <Text style={styles.found}>SE ENCONTRÓ {vehicleList.length} RESULTADOS</Text>
-              <ScrollView>
-                {
-                  vehicleList.map((item, i) => (
-                    
-                    <ListItem
-                      key={i}
-                      Component={TouchableScale}
-                      friction={90}
-                      tension={100}
-                      activeScale={0.95}
-                      title={item.plate}
-                      titleStyle={{ fontWeight: 'bold' }}
-                      subtitleStyle={{ textTransform: 'uppercase' }}
-                      subtitle={`${item.brand} - ${item.color}`}
-                      disabled={spinner}
-                      leftIcon={
-                        <Avatar
-                          size="medium"
-                          overlayContainerStyle={{backgroundColor: '#000'}}
-                          rounded 
-                          icon={{ name: 'car', color: '#b98700', type: 'material-community' }} 
-                        />
-                      }
-                      bottomDivider
-                      chevron
-                    />
-                  ))
-                }
-              </ScrollView>
-              <View style={{marginBottom: 30}} />
-            </>
-          )
-        )}
-        {search.length > 4 && vehicleList.length === 0 && (
-          <Button
-            label="Continuar"
-            // handleSubmit={handleSubmit}
-            // disabled={isLoading}
+    const { step, search, spinner, vehicleList, customerList } = this.state;
+    
+    if (step === 1) {
+      return (
+        <View style={{flex: 1, paddingHorizontal: 30 }}>
+          <Text style={styles.title}>PASO 1</Text>
+          <SearchBar
+            placeholder="Digite la placa..."
+            onChangeText={this.updateSearch}
+            value={search}
+            showLoading={spinner}
+            inputStyle={styles.textUppercase}
           />
-        )}
-      </View>
-    )
+  
+          <Text style={styles.plate}>{search}</Text>
+          {search.length > 2 && (
+            vehicleList.length === 0 ? (
+              <>
+                <Text style={styles.found}>NO SE ENCONTRARON RESULTADOS</Text>
+                {search.length < 5 && <Text style={styles.found}>Escribe al menos 5 caracteres para continuar...</Text>}
+              </>
+            ) : (
+              <>
+                <Text style={styles.found}>SE ENCONTRO {vehicleList.length} VEHICULOS</Text>
+                <ScrollView>
+                  {
+                    vehicleList.map((item, index) => (
+                      
+                      <ListItem
+                        key={index}
+                        Component={TouchableScale}
+                        friction={90}
+                        tension={100}
+                        activeScale={0.95}
+                        title={item.plate}
+                        titleStyle={{ fontWeight: 'bold' }}
+                        subtitleStyle={{ textTransform: 'uppercase' }}
+                        subtitle={`${item.brand} - ${item.color}`}
+                        disabled={spinner}
+                        leftIcon={
+                          <Avatar
+                            size="medium"
+                            overlayContainerStyle={{backgroundColor: '#000'}}
+                            rounded 
+                            icon={{ name: 'car', color: '#b98700', type: 'material-community' }} 
+                          />
+                        }
+                        onPress={() => this.handleSelectVehicle(index)}
+                        bottomDivider
+                        chevron
+                      />
+                    ))
+                  }
+                </ScrollView>
+                <View style={{marginBottom: 30}} />
+              </>
+            )
+          )}
+          {search.length > 4 && vehicleList.length === 0 && (
+            <Button
+              label="Continuar"
+              // handleSubmit={handleSubmit}
+              // disabled={isLoading}
+            />
+          )}
+        </View>
+      )
+    }
+
+    if (step === 2) {
+      return (
+        <View style={{flex: 1, paddingHorizontal: 30 }}>
+          <Text style={styles.title}>PASO 2</Text>
+          <Text style={styles.found}>SE ENCONTRO {customerList.length} CLIENTES</Text>
+          <ScrollView>
+            {
+              customerList.map((item, i) => (
+                <ListItem
+                  key={i}
+                  Component={TouchableScale}
+                  friction={90}
+                  tension={100}
+                  activeScale={0.95}
+                  title={item.name}
+                  titleStyle={{ fontWeight: 'bold' }}
+                  subtitleStyle={{ textTransform: 'uppercase' }}
+                  subtitle={`${item.document_type} - ${item.document_number}`}
+                  disabled={spinner}
+                  leftIcon={
+                    <Avatar
+                      size="medium"
+                      overlayContainerStyle={{backgroundColor: '#b98700'}}
+                      rounded 
+                      icon={{ name: 'account', color: '#000', type: 'material-community' }} 
+                    />
+                  }
+                  bottomDivider
+                  chevron
+                />
+              ))
+            }
+          </ScrollView>
+          <View style={{marginBottom: 30}} />
+        </View>
+      )
+    }
+    
   }
 }
 
@@ -146,6 +186,14 @@ const styles = StyleSheet.create({
     color: '#969696',
     textAlign: 'center',
     marginBottom: 20,
+  },
+  numberText: {
+    backgroundColor: '#b98700',
+    color: '#fff',
+    width: 100,
+    height: 40,
+    fontSize: 20,
+    
   },
   textUppercase: {
     textTransform: 'uppercase',
