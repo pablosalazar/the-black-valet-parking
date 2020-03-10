c6d9adba65ed0b39112d0a64c22a3844d85b73fa
Juan Pablo Salazar <pablo.salazar.restrepo@gmail.com>
2020-03-10 11:55:59 -0500

    Create Alert component

--- components/index.js
+++ components/index.js
@@ -2,10 +2,12 @@ import Input from './Input';
 import Select from './Select';
 import Button from './Button';
 import Loader from './Loader';
+import AlertCustom from './Alert';
 
 export {
   Button,
   Input,
   Select,
   Loader,
+  AlertCustom,
 };
